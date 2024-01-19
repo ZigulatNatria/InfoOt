b52b27a467be7b7b5cc19778e345b93aadfb89d1
ZigulatNatria <vachrameev.oleg@yandex.ru>
2024-01-19 17:56:34 +0500

    Переписан выход из системы под django 5

--- InfoOt/sign/urls.py
+++ InfoOt/sign/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import UserAddView
+from .views import UserAddView, logout_view
 
 urlpatterns = [
     path('login/',
          LoginView.as_view(template_name = 'sign/login.html'),
          name='login'),
-    path('logout/',
-         LogoutView.as_view(template_name = 'sign/logout.html'),
-         name='logout'),
+    path('logout/', logout_view, name='logout'),
     path('signup/',
          UserAddView.as_view(template_name='sign/signup.html'),
          name='signup'),
--- InfoOt/sign/views.py
+++ InfoOt/sign/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic.edit import CreateView
 from info.models import Employee
 from .forms import UserAddForm
-
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 class UserAddView(PermissionRequiredMixin, CreateView):
     permission_required = (
@@ -11,3 +12,8 @@ class UserAddView(PermissionRequiredMixin, CreateView):
     model = Employee
     template_name = 'create.html'
     form_class = UserAddForm
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
