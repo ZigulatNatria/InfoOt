847a1ba3cb373a1d94a89ed11c47f96db679a7d8
ZigulatNatria <vachrameev.oleg@yandex.ru>
2024-01-22 17:05:20 +0500

    разделены доступы для 2-х групп админ и кадровики сделана ссылка на работника со страницы просроченных удостоверений

--- InfoOt/info/context_processors.py
+++ InfoOt/info/context_processors.py
@@ -2,3 +2,7 @@
 
 def admin_user(request):
     return {'admin_user': request.user.groups.filter(name='admin').exists()}
+
+
+def hr_user(request):
+    return {'hr_user': request.user.groups.filter(name='hr').exists()}
